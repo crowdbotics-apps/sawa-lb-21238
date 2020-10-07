ad88ea588fff0ddedb12e19dc2b467bf527c5653
Crowdbotics <team@crowdbotics.com>
2020-10-07 23:23:11 +0000

    Update models

--- backend/sawa_lb_21238/wsgi.py
+++ backend/sawa_lb_21238/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sawa_lb_21238.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sawa_lb_21238.settings")
 
 application = get_wsgi_application()
